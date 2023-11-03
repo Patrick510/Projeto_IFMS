fab7c67b75a3c486e731916a6add8f45e51cf1f2
unknown <patrickdedinho321xz@gmail.com>
2023-11-03 17:07:14 -0400

    project_commit04

--- Spring/produto/produto/src/main/java/com/api/produto/repositorio/ProdutoRepositorio.java
+++ Spring/produto/produto/src/main/java/com/api/produto/repositorio/ProdutoRepositorio.java
@@ -1,24 +1,21 @@
 package com.api.produto.repositorio;
 
 import java.util.List;
+import com.api.produto.modelo.ProdutoModelo;
 
 import org.springframework.data.repository.CrudRepository;
-
-import com.api.produto.modelo.ProdutoModelo;
 
 public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Integer>{
 	
 	//Listar todos os produtos
 	List<ProdutoModelo> findAll();
 	
-	
 	//Pesquisar por codigo
-	ProdutoModelo findbyCodigo(int codigo);
+	ProdutoModelo findByCodigo(int codigo);
 	
 	//Remover produto
 	void delete(ProdutoModelo produto);
 	
-	//Cadastrar/Alterarproduto
+	//Cadastrar/Alterarprodutoo
 	ProdutoModelo save(ProdutoModelo produto);
-
 }
